12406a6f977396320b26b9523ca904bfba091fb3
Varunkumar1408 <156279699+Varunkumar1408@users.noreply.github.com>
2024-07-23 10:13:04 +0000

    02_datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,11 @@
 "use strict";
+
+console.log(3+3);
 console.log("varun");
+
+let name = "varunn"
+let age =24
+let isLoggiedIn = true
+console.log(typeof "varunn");
+console.log(typeof age);
+console.log(typeof null);
