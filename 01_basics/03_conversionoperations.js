9c61ad91e8bbbef88be48643a78823c742c0a62c
Varunkumar1408 <156279699+Varunkumar1408@users.noreply.github.com>
2024-07-26 09:21:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,49 @@
+// let score = "33"
+
+// console.log(typeof score); //string
+// console.log(typeof (score)); //string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //33
+
+
+// let score = "33abc"
+
+// console.log(typeof score); //string
+// console.log(typeof (score)); //string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //NaN(notanumber)
+
+// let score = true
+
+// console.log(typeof score); //boolean
+// console.log(typeof (score)); //boolean
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber); //1
+
+// let score = false
+
+// console.log(typeof score); //boolean
+// console.log(typeof (score)); //boolean
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //0
+
+// let isLoggedIn = 1
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true, 0=> false
+// "" => false, 
+// "hitesh" => true
+
+let someNumber = 33
+let someString = String(someNumber)
+console.log(someString); //33
+console.log(typeof someString); //string
