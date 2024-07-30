60ea66e8311c510875e166db96dafe32ecae6194
Varunkumar1408 <156279699+Varunkumar1408@users.noreply.github.com>
2024-07-30 17:07:19 +0000

    dates and time

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,13 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString);
+console.log(typeof myDate);
+
+//let myCreatedate = new Date(2023,0,23,5,3)
+let myCreatedate = new Date("2023-01-14")
+
+console.log(myCreatedate.toLocaleString());
