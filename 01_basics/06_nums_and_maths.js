9bde844c11ac9000300553d3949ea857e8764e76
Varunkumar1408 <156279699+Varunkumar1408@users.noreply.github.com>
2024-07-30 08:31:49 +0000

    numbers and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,25 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+
+const num = 23.897
+//console.log(num.toPrecision(3));
+//console.log(num.toPrecision(2));
+
+const num1 = 1000000
+//console.log(num1.toLocaleString()); //american standards
+//console.log(num1.toLocaleString('en-IN')); //indian standards
+
+// ++++++++++++++++++++ MATH +++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,2,7,5,9));
+console.log(Math.max(4,2,7,5,9));
