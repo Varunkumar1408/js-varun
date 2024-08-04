14a5a3094a7770588a13542acd333fa82c1f3a27
Varunkumar1408 <156279699+Varunkumar1408@users.noreply.github.com>
2024-08-04 11:08:14 +0000

    For-Of loop

---
+++ 05_iterations/03.js
@@ -0,0 +1,41 @@
+// for of
+
+// ["" , "" , ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+ for (const num of arr) {
+   // console.log(num);
+}
+
+const greetings = "hello, world!"
+for (const greet of greetings) {
+    if (greetings=="") {
+        continue;
+    }
+    //console.log(`Each char is ${greet}`);
+}
+
+//Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "united states of america")
+map.set('Fr', "france")
+map.set('IN', "India")
+//console.log(map);
+
+for (const [key,value] of map) {
+   // console.log(key, ':-' , value);
+}
+
+const myObject = {
+    value1 : 'varun',
+    value2 : 'kumar'
+}
+// for (const [key,value] of myObject) {
+//     console.log(key, ':-' , value);
+// }
+
+//// NOTE :- WE CANNOT ITERATE OBJECTS INSIDE FOR-OF LOOP
